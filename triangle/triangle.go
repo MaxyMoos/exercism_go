ddec692114f1749916eb86ca6cbd50134bb68725
MaxyMoos <ohouiduspam@gmail.com>
2016-01-28 19:11:21 +0100

    - Adding triangles

--- triangle/triangle.go
+++ triangle/triangle.go
@@ -1,17 +1,37 @@
-//+build !example
-
 package triangle
 
-// Code this function.
-func KindFromSides(a, b, c float64) Kind
+// Required for using IsNaN function
+import "math"
 
-// Notice it returns this type.  Pick something suitable.
-type Kind
+func KindFromSides(a, b, c float64) Kind {
+    // First part of the function does the mandatory tests
+    if math.IsNaN(a) || math.IsNaN(b) || math.IsNaN(c) {
+		return NaT
+	}
+	if a <= 0 || b <= 0 || c <= 0 {
+		return NaT
+	}
+	if a+b <= c || a+c <= b || b+c <= a {
+		return NaT
+	}
 
-// Pick values for the following identifiers used by the test program.
-Equ // equilateral
-Iso // isosceles
-Sca // scalene
-NaT // not a triangle
+    // If triangle is possible, second part identifies the kind
+	if a == b && a == c {
+		return Equ
+	} else if a == b || a == c || b == c {
+		return Iso
+	} else {
+		return Sca
+	}
+}
 
-// Organize your code for readability.
+// Just an int shall suffice
+type Kind int
+
+// Just learned about the existence of 'iota', good tool for enums
+const (
+	Equ Kind = iota // equilateral
+	Iso Kind = iota // isosceles
+	Sca Kind = iota // scalene
+	NaT Kind = iota // not a triangle
+)
